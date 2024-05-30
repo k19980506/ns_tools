8a29715f2aa229b3f5b3ab17513901fb2d88e8f4
King <k19980506@gmail.com>
2024-05-30 14:32:22 +0800

    tmp

--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,56 +1,111 @@
-import { Autocomplete } from "@react-google-maps/api";
 import { useState, useRef } from "react";
 import { Input } from "antd";
+import { LoaderCircle } from "lucide-react";
 
 const SearchInput = ({ map, isLoaded }) => {
-  const [searchInput, setSearchInput] = useState("");
-  const autoCompleteRef = useRef(null);
+  const [searchValue, setSearchValue] = useState("");
+  const [showPredictions, setShowPredictions] = useState(false);
+  const [predictions, setPredictions] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const handleLoad = (autoComplete) => {
-    autoCompleteRef.current = autoComplete;
-  };
-
-  const onPlaceChanged = () => {
-    if (autoCompleteRef.current !== null) {
-      const place = autoCompleteRef?.current.getPlace();
-      if (place && place.geometry && place.geometry.location) {
-        const lat = place.geometry.location.lat();
-        const lng = place.geometry.location.lng();
-        setSearchInput(place.name);
-        map?.setCenter({ lat, lng });
-      } else {
-      }
+  const getPlaceInfo = (placeId) => {
+    if (!window?.google?.maps?.places) return;
+    let service = new window.google.maps.places.PlacesService(map);
+    if (service && service?.getDetails) {
+      service.getDetails({ placeId }, (place, status) => {
+        if (!place && status !== "OK") {
+          console.info(status);
+          return;
+        }
+        if (!place.geometry) return;
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(17);
+        }
+        setPredictions([]);
+        setSearchValue("");
+        setShowPredictions(false);
+      });
     }
   };
 
-  const handleInputChange = (e) => {
-    const value = e?.target.value;
-    setSearchInput(value);
-  };
+  const handleSearchChange = async (value) => {
+    setLoading(true);
+    setShowPredictions(true);
+    setSearchValue(value);
+    if (!window?.google?.maps?.places) return;
+    const service = new window.google.maps.places.AutocompleteService();
 
-  const options = {
-    componentRestrictions: { country: "tw" },
+    service.getPlacePredictions({ input: value }, (predictions) => {
+      console.log(predictions);
+      if (predictions) {
+        const newPrediction = { place_id: "", description: value };
+        setPredictions([newPrediction, ...predictions]);
+        setLoading(false);
+      }
+      return;
+    });
+    // if (
+    //   AutocompleteService &&
+    //   AutocompleteService?.getPlacePredictions({ input: value })
+    // ) {
+    //   AutocompleteService.getPlacePredictions(
+    //     { value },
+    //     function showPredictions(res, status) {
+    //       console.log("res", res);
+    //       if (!res && status !== "OK") {
+    //         setLoading(false);
+    //         return;
+    //       }
+    //       let predictionResponse = [];
+    //       if (res && res.length > 0) {
+    //         predictionResponse = res.map((place) => {
+    //           return {
+    //             description: place.description,
+    //             matchedSubString: place.matched_substrings,
+    //             placeId: place.place_id,
+    //           };
+    //         });
+    //       }
+    //       setPredictions(predictionResponse);
+    //       setLoading(false);
+    //     },
+    //   );
+    // }
   };
 
   return (
     <div className="w-screen">
-      {isLoaded ? (
-        <Autocomplete
-          onLoad={handleLoad}
-          onPlaceChanged={onPlaceChanged}
-          options={options}
-        >
-          <div className="flex">
-            Location:
-            <Input
-              value={searchInput}
-              onChange={handleInputChange}
-              placeholder="輸入地址或地標..."
-            />
-          </div>
-        </Autocomplete>
-      ) : (
-        <div>正在載入中</div>
+      <div className="flex">
+        Location:
+        <Input
+          type="text"
+          value={searchValue}
+          onChange={(e) => handleSearchChange(e.target.value)}
+          placeholder="輸入地址或地標..."
+        />
+      </div>
+      {searchValue && showPredictions && (
+        <ul>
+          {loading && (
+            <li>
+              <LoaderCircle />
+            </li>
+          )}
+          {!loading &&
+            predictions.length > 0 &&
+            predictions.map((place) => (
+              <li
+                key={place.place_id}
+                onClick={() => getPlaceInfo(place.place_id)}
+              >
+                <p>{place.description}</p>
+              </li>
+            ))}
+          {!loading && !predictions.length && <li>No results</li>}
+        </ul>
       )}
     </div>
   );
